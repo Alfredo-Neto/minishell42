7dbdf5a16c46b13d627976649b0b92899f2d8804
alezamarion <alezamarion@icloud.com>
2022-01-31 21:58:49 -0300

    first lines

--- src/minishell.c
+++ src/minishell.c
@@ -1,15 +1,40 @@
-eae Jorge
-eae Alexandre
-e ae Alfredo
-eae baratão
 
+#include "../includes/minishell.h"
 
+// Greeting shell during startup
+void init_shell()
+{
+    clear();
+    printf("\n\t****MY SHELL****\n");
+    printf("\n\t-USE AT YOUR OWN RISK-\n");
+    printf("\n*******************"
+        "***********************");
+    char* username = getenv("USER");
+    printf("\n\n\nUSER is: @%s", username);
+    printf("\n");
+    sleep(10);
+    clear();
+}
 
+// Function to take input
+int takeInput(char* str)
+{
+    char* buf;
+  
+    buf = readline("\n>>> ");
+    if (strlen(buf) != 0) {
+        add_history(buf);
+        strcpy(str, buf);
+        return 0;
+    } else {
+        return 1;
+    }
+}
 
-FAAAAAAAALA MEUS QUERIDOS!
+int main(void)
+{
 
+    init_shell();
+    return (0);
 
-VAMO COMECAR ESSE PROJETO!!!!
-
-Ultimo do dia
-Agora é a ultima mesmo!
+}
