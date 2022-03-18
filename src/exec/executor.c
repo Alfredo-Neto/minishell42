cfe3d295e665308a7a48862782ad188999256dea
Alfredo Neto <alfredoneto934@gmail.com>
2022-03-17 22:58:42 -0300

    created echo builtin

--- src/exec/executor.c
+++ src/exec/executor.c
@@ -13,15 +13,15 @@
 #include "../../minishell.h"
 
 // Function where the system command is executed
-void	exec_args(char **parsed)
+void exec_args(char **parsed)
 {
-	pid_t	pid;
+	pid_t pid;
 
 	pid = fork();
 	if (pid == -1)
 	{
 		printf("\nFailed forking child..");
-		return ;
+		return;
 	}
 	else if (pid == 0)
 	{
@@ -34,27 +34,27 @@ void	exec_args(char **parsed)
 	else
 	{
 		wait(NULL);
-		return ;
+		return;
 	}
 }
 
 // Function where the piped system commands is executed
-void	exec_args_piped(char **parsed, char **parsedpipe)
+void exec_args_piped(char **parsed, char **parsedpipe)
 {
-	int		pipefd[2];
-	pid_t	p1;
-	pid_t	p2;
+	int pipefd[2];
+	pid_t p1;
+	pid_t p2;
 
 	if (pipe(pipefd) < 0)
 	{
 		printf("\nPipe could not be initialized");
-		return ;
+		return;
 	}
 	p1 = fork();
 	if (p1 < 0)
 	{
 		printf("\nCould not fork");
-		return ;
+		return;
 	}
 	if (p1 == 0)
 	{
@@ -73,7 +73,7 @@ void	exec_args_piped(char **parsed, char **parsedpipe)
 		if (p2 < 0)
 		{
 			printf("\nCould not fork");
-			return ;
+			return;
 		}
 		if (p2 == 0)
 		{
@@ -95,44 +95,45 @@ void	exec_args_piped(char **parsed, char **parsedpipe)
 }
 
 // Help command builtin
-void	open_help(void)
+void open_help(void)
 {
 	puts("\n***WELCOME TO MY SHELL HELP***"
-		"\nCopyright @ Suprotik Dey"
-		"\n-Use the shell at your own risk..."
-		"\nList of Commands supported:"
-		"\n>cd"
-		"\n>ls"
-		"\n>exit"
-		"\n>all other general commands available in UNIX shell"
-		"\n>pipe handling"
-		"\n>improper space handling");
-	return ;
+		 "\nCopyright @ Suprotik Dey"
+		 "\n-Use the shell at your own risk..."
+		 "\nList of Commands supported:"
+		 "\n>cd"
+		 "\n>ls"
+		 "\n>exit"
+		 "\n>all other general commands available in UNIX shell"
+		 "\n>pipe handling"
+		 "\n>improper space handling");
+	return;
 }
 
 // Function to execute builtin commands
-int	own_cmd_handler(char **parsed)
+int own_cmd_handler(char **parsed)
 {
-	int		i;
-	int		switch_own_arg;
-	char	*list_of_own_cmds[4];
-	char	*username;
+	int i;
+	int switch_own_arg;
+	char *list_of_own_cmds[4];
+	char *username;
 
 	i = 0;
 	switch_own_arg = 0;
 	list_of_own_cmds[0] = "exit";
 	list_of_own_cmds[1] = "cd";
-	list_of_own_cmds[2] = "help";
+	list_of_own_cmds[2] = "echo";
 	list_of_own_cmds[3] = "hello";
 	while (i < 4)
 	{
 		if (strcmp(parsed[0], list_of_own_cmds[i]) == 0)
 		{
 			switch_own_arg = i + 1;
-			break ;
+			break;
 		}
 		i++;
 	}
+
 	if (switch_own_arg == 1)
 	{
 		printf("\nGoodbye\n");
@@ -145,14 +146,15 @@ int	own_cmd_handler(char **parsed)
 	}
 	else if (switch_own_arg == 3)
 	{
-		open_help();
+		echo(&parsed[1]);
 		return (1);
 	}
 	else if (switch_own_arg == 4)
 	{
 		username = getenv("USER");
 		printf("\nHello %s.\nMind that this is not a place to play around."
-			"\nUse help to know more..\n", username);
+			   "\nUse help to know more..\n",
+			   username);
 		return (1);
 	}
 	return (0);
