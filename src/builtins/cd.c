044b2a87b13c29fab4904a8bdbe3833d49db062f
Alfredo Neto <alfredoneto934@gmail.com>
2022-03-19 04:31:24 -0300

    some adjustments

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,22 +1,37 @@
 #include "../../minishell.h"
 
-static char *trim_backwards_until_slash(char *absolute_path)
+static char *trim_backwards_until_slash(char **absolute_path)
 {
     size_t i;
 
-    i = ft_strlen(absolute_path);
-    while (absolute_path[i] != '/')
+    printf("DENTRO DO TRIM %s\n", *absolute_path);
+    i = ft_strlen(*absolute_path);
+    while ((*absolute_path)[i] != '/')
+    {
+        printf("DENTRO DO WHILE\n");
         i--;
-    return (ft_substr(absolute_path, 0, i + 1));
+    }
+    *absolute_path = ft_substr(*absolute_path, 0, i + 1);
+    printf("ABS %s\n", *absolute_path);
+    return (*absolute_path);
 }
 
 static char **trim_off_current_dir(char **directory)
 {
-    char *absolute_path;
-    absolute_path = ft_strdup("");
-    absolute_path = "/home/alfredo/42-projects/minishell42";
-
-    *directory = trim_backwards_until_slash(absolute_path);
+    printf("DIR %s\n", *directory);
+    static char *absolute_path;
+    if (absolute_path == NULL)
+    {
+        printf("DENTRO DO ELSE %s\n", absolute_path);
+        absolute_path = "PWD";
+        absolute_path = getenv(absolute_path);
+        *directory = trim_backwards_until_slash(&absolute_path);
+    }
+    else
+    {
+        printf("DENTRO DO IF %s\n", absolute_path);
+        *directory = trim_backwards_until_slash(&absolute_path);
+    }
     return (directory);
 }
 
