686b76210186881229adb6db05a8221c579a5d44
lilangbr <eblang@gmail.com>
2022-05-15 23:24:33 -0300

    fixing 0 - normless

--- src/EXPAND/expand_variables.c
+++ src/EXPAND/expand_variables.c
@@ -6,7 +6,7 @@
 /*   By: ebresser <ebresser@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 15:34:28 by ebresser          #+#    #+#             */
-/*   Updated: 2022/05/15 19:11:42 by ebresser         ###   ########.fr       */
+/*   Updated: 2022/05/15 23:17:39 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,9 +73,13 @@ static char	*pull_var_value(char **s, t_vars *vars)
 	while (s[0][offset] != '$')
 		offset++;
 	i = offset + 1;
-	while (ft_isalpha(s[0][i]) || ft_isdigit(s[0][i]) || s[0][i] == '_' || \
-		(s[0][i] == '?' && i == offset + 2))
+	if (s[0][i] == '?')
 		i++;
+	else
+	{
+		while (ft_isalpha(s[0][i]) || ft_isdigit(s[0][i]) || s[0][i] == '_')
+			i++;
+	}		
 	var_name = ft_substr(s[0], offset, i - offset);
 	vdt = find_in_list(var_name, vars);
 	free(var_name);
